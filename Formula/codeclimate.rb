304b72ea911168620cbe0b123eeeb0f9f9a1b073
GordonDiggs <gordon@gordondiggs.com>
2015-06-19 13:18:51 -0400

    Bump version

--- Formula/codeclimate.rb
+++ Formula/codeclimate.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Codeclimate < Formula
   homepage "https://github.com/codeclimate/codeclimate"
-  version "0.0.5"
+  version "0.0.5-1"
 
   url "https://github.com/codeclimate/codeclimate/archive/v0.0.5.tar.gz"
   sha1 "1252d6ec10b579a4d249ff99d1c95977e875f2d3"

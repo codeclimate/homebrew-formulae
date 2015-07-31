d93fb60896c86b3a225807e76300f18b40913c70
Gordon Diggs <gordon@gordondiggs.com>
2015-07-31 17:15:07 -0400

    Release 0.2.5

--- Formula/codeclimate.rb
+++ Formula/codeclimate.rb
@@ -1,13 +1,13 @@
 require "formula"
 
 class Codeclimate < Formula
-  CODECLIMATE_VERSION = "0.2.4".freeze
+  CODECLIMATE_VERSION = "0.2.5".freeze
 
   homepage "https://github.com/codeclimate/codeclimate"
   version CODECLIMATE_VERSION
 
   url "https://github.com/codeclimate/codeclimate/archive/v#{CODECLIMATE_VERSION}.tar.gz"
-  sha1 "38cbf05a6af828da7c1d95ee9421d38198d70ed4"
+  sha1 "fa6b63e338c33c703e5b2d98b028f5ee643de64c"
 
   def install
     # Alter PATH to ensure `docker' is available

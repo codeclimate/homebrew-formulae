375897b9eba8baea0d448f09c9a418e91edc1e1c
patrick brisbin <pbrisbin@gmail.com>
2015-08-24 09:11:22 -0400

    Correct sha1 for v0.2.12

--- Formula/codeclimate.rb
+++ Formula/codeclimate.rb
@@ -7,7 +7,7 @@ class Codeclimate < Formula
   version CODECLIMATE_VERSION
 
   url "https://github.com/codeclimate/codeclimate/archive/v#{CODECLIMATE_VERSION}.tar.gz"
-  sha1 "d205cbd6783332a212c5ae92d73c77178c2d2f28"
+  sha1 "c52cbdf349841eddc3ce6697885a25a58f285ad5"
 
   def install
     # Alter PATH to ensure `docker' is available

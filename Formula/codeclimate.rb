d34f2a7171d62c4b3abcc75d570928cf627dedf1
Andy Blyler <andy@blyler.cc>
2016-04-05 22:44:57 -0400

    Fix homebrew audit strict warnings
    - add description
    - migrate from sha1 to sha256
    - remove version, since it is parsed from URL
    - remove calls to env, and use ENV instead
    - remove require formula

--- Formula/codeclimate.rb
+++ Formula/codeclimate.rb
@@ -1,19 +1,18 @@
-require "formula"
-
 class Codeclimate < Formula
-  CODECLIMATE_VERSION = "0.24.3".freeze
-
+  desc "Code Climate CLI"
   homepage "https://github.com/codeclimate/codeclimate"
-  version CODECLIMATE_VERSION
-
-  url "https://github.com/codeclimate/codeclimate/archive/v#{CODECLIMATE_VERSION}.tar.gz"
-  sha1 "89da48bcc6e44a3546c0b730c4e2cc7535e79696"
+  url "https://github.com/codeclimate/codeclimate/archive/v0.24.3.tar.gz"
+  sha256 "70591e62a1c1dd61a56f133d0c0849fd94cfebf146bbdec765a9e01fffa75640"
 
   def install
     # Alter PATH to ensure `docker' is available
-    system "env", \
-      "PATH=#{HOMEBREW_PREFIX}/bin:#{ENV["PATH"]}", \
-      "PREFIX=#{prefix}", "make", "install"
+    if Formula["docker"].linked_keg.exist?
+      ENV.prepend_path "PATH", Formula["docker"].opt_bin
+    end
+
+    ENV["PREFIX"] = prefix
+
+    system "make", "install"
   end
 
   test do

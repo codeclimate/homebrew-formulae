28f3cb4c23fa64cdd903c47a0f49746d83fe5afb
Devon Blandin <dblandin@gmail.com>
2016-11-04 17:05:21 -0400

    Release v0.40.0

--- Formula/codeclimate.rb
+++ Formula/codeclimate.rb
@@ -1,8 +1,8 @@
 class Codeclimate < Formula
   desc "Code Climate CLI"
   homepage "https://github.com/codeclimate/codeclimate"
-  url "https://github.com/codeclimate/codeclimate/archive/v0.39.0.tar.gz"
-  sha256 "89cc4f58da12da7f14b749500455800ac131745e70a3820775e76ab5cc78a536"
+  url "https://github.com/codeclimate/codeclimate/archive/v0.40.0.tar.gz"
+  sha256 "c908611e667a51555dbe4190bc02f0ffaea749e5c93d36220ff0ded96d5c17ad"
 
   def install
     # Alter PATH to ensure `docker' is available

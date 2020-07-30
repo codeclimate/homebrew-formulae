7443ffeacef52968d0dec09ee45f981037d44f60
Filipe Esperandio <filipesperandio@gmail.com>
2020-07-30 16:03:05 -0300

    Release v0.85.13

--- Formula/codeclimate.rb
+++ Formula/codeclimate.rb
@@ -1,8 +1,8 @@
 class Codeclimate < Formula
   desc "Code Climate CLI"
   homepage "https://github.com/codeclimate/codeclimate"
-  url "https://github.com/codeclimate/codeclimate/archive/v0.85.12.tar.gz"
-  sha256 "9e2b20116c76905fc40fc6e0892629e916d4aa9d9f5b79b3c8bc6d4ffe021a37"
+  url "https://github.com/codeclimate/codeclimate/archive/v0.85.13.tar.gz"
+  sha256 "66610e87d8fdaf026d661680cbcbf9feb5646e91bb2409b9ecb9edf618c24f0f"
 
   def install
     # Alter PATH to ensure `docker' is available

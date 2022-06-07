6f222f662335a2b80fc049ad2781ea0e3b7802c8
GitHub Action <github-action@users.noreply.github.com>
2022-06-07 20:28:33 +0000

    Release v0.85.28

--- Formula/codeclimate.rb
+++ Formula/codeclimate.rb
@@ -1,8 +1,8 @@
 class Codeclimate < Formula
   desc "Code Climate CLI"
   homepage "https://github.com/codeclimate/codeclimate"
-  url "https://github.com/codeclimate/codeclimate/archive/v0.85.27.tar.gz"
-  sha256 "94582d333f0db5182360f0b850a9e00b8b1c51d31d2d84a047a581bcf46236cd"
+  url "https://github.com/codeclimate/codeclimate/archive/v0.85.28.tar.gz"
+  sha256 "3cda24dec0d3e029831d932284a7442c6ed948a5cd6880971bd55b7c454ade48"
 
   def install
     # Alter PATH to ensure `docker' is available
